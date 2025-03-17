57641f422f627ff8030e03797f0df46624371b31
dashka0212 <44989745+dashka0212@users.noreply.github.com>
2025-03-17 18:40:14 +0800

    image placeholder

--- src/components/gallery/image-item.tsx
+++ src/components/gallery/image-item.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { useState, useEffect, useRef } from "react";
-import { CldImage } from "next-cloudinary";
+import { CldImage, getCldImageUrl } from "next-cloudinary";
 
 export function ImageItem({ image }: { image: CloudinaryImage }) {
   const containerRef = useRef<HTMLDivElement>(null);
+  const [placeholder, setPlaceholder] = useState("data:png;base64,a");
   const [containerWidth, setContainerWidth] = useState(300);
 
   useEffect(() => {
@@ -20,6 +21,24 @@ export function ImageItem({ image }: { image: CloudinaryImage }) {
     return () => window.removeEventListener("resize", updateWidth);
   }, []);
 
+  useEffect(() => {
+    getPlaceholder().then((c) => setPlaceholder(c));
+  }, [image]);
+
+  const getPlaceholder = async () => {
+    const imageUrl = getCldImageUrl({
+      src: image.public_id,
+      width: 10,
+      blur: "5",
+      quality: "auto:low",
+    });
+    const response = await fetch(imageUrl);
+    const arrayBuffer = await response.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+    const base64 = buffer.toString("base64");
+    return `data:${response.type};base64,${base64}`;
+  };
+
   return (
     <div
       ref={containerRef}
@@ -31,7 +50,9 @@ export function ImageItem({ image }: { image: CloudinaryImage }) {
         width={containerWidth}
         height={432}
         quality="auto:low"
-        className="h-full w-full object-cover transition-all duration-200 group-hover/image:scale-105"
+        placeholder="blur"
+        blurDataURL={placeholder}
+        className="h-full w-full bg-slate-900 object-cover transition-all duration-200 group-hover/image:scale-105"
       />
     </div>
   );
